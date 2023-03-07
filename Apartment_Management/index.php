73154904d16db586b93a9b5d3e3ec6c192b03b23
shreya37 <93190521+shreya37@users.noreply.github.com>
2023-03-07 16:36:09 +0530

    Update index.php

--- Apartment_Management/index.php
+++ Apartment_Management/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     
   <head>
-  <title>Apartment Management</title>
+  <title>Apartment Management System</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
